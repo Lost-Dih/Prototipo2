122a314a59d31acafb9cca8f338184fda74031da
Diana <dfranciscamagalhaes@gmail.com>
2026-03-06 12:16:33 +0000

    Lesson 2.1 - Player Positioning

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //keep the player in bounds
+       if( transform.position.x < -xRange){
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+       }
+       if(transform.position.x > xRange){
+        transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+       }
+       horizontalInput = Input.GetAxis("Horizontal");
+       transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
